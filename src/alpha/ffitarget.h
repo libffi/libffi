f41bec3b576aa5ff8915b1188446c2dc086dfe64
Richard Henderson <rth@twiddle.net>
2014-10-17 20:46:48 -0700

    alpha: Add support for complex types

--- src/alpha/ffitarget.h
+++ src/alpha/ffitarget.h
@@ -44,6 +44,9 @@ typedef enum ffi_abi {
 } ffi_abi;
 #endif
 
+#define FFI_TARGET_SPECIFIC_STACK_SPACE_ALLOCATION
+#define FFI_TARGET_HAS_COMPLEX_TYPE
+
 /* ---- Definitions for closures ----------------------------------------- */
 
 #define FFI_CLOSURES 1
--- src/alpha/internal.h
+++ src/alpha/internal.h
@@ -2,6 +2,8 @@
 #define ALPHA_ST_INT	1
 #define ALPHA_ST_FLOAT	2
 #define ALPHA_ST_DOUBLE	3
+#define ALPHA_ST_CPLXF	4
+#define ALPHA_ST_CPLXD	5
 
 #define ALPHA_LD_VOID	0
 #define ALPHA_LD_INT64	1
@@ -12,6 +14,8 @@
 #define ALPHA_LD_SINT8	6
 #define ALPHA_LD_FLOAT	7
 #define ALPHA_LD_DOUBLE	8
+#define ALPHA_LD_CPLXF	9
+#define ALPHA_LD_CPLXD	10
 
 #define ALPHA_ST_SHIFT		0
 #define ALPHA_LD_SHIFT		8
