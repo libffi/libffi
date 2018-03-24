e5de3bb28df43ddb203a6e6edafe80fd95449b78
Anthony Green <green@moxielogic.com>
2018-03-24 09:33:12 -0400

    Use travis build script

---
+++ .travis/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# exit this script if any commmand fails
+set -e
+
+function build_linux()
+{
+    ./autogen.sh
+    ./configure ${HOST+--host=$HOST} ${CONFIGURE_OPTIONS}
+    make
+    make dist
+    make check RUNTESTFLAGS="-a $RUNTESTFLAGS"
+    cat */testsuite/libffi.log
+}
+
+function build_ios()
+{
+    which python
+# export PYTHON_BIN=/usr/local/bin/python
+    ./generate-darwin-source-and-headers.py
+    xcodebuild -project libffi.xcodeproj -target "libffi iOS" -configuration Release -sdk iphoneos6.1 OTHER_CFLAGS="-no-integrated-as"
+}
+
+case "$HOST" in
+    arm-apple-darwin*)
+	build_ios
+	;;
+    *)
+	build_linux
+	;;
+esac
