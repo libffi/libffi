a7d6396f06d5e1a726f24b746b509514f466380f
Dan Horák <dan@danny.cz>
2019-03-29 14:19:20 +0100

    fix check for Linux/aarch64
    fixes #473

--- src/aarch64/ffi.c
+++ src/aarch64/ffi.c
@@ -19,7 +19,7 @@ CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 
-#ifdef __arm64__
+#if defined(__aarch64__) || defined(__arm64__)
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
