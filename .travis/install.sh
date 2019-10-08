25cf9cc4a5ce4a272252de3f681d60dafd838ceb
Anthony Green <green@moxielogic.com>
2019-10-08 06:24:55 -0400

    Use https for moxie toolchain repo

--- .travis/install.sh
+++ .travis/install.sh
@@ -14,11 +14,11 @@ else
 	sudo apt-get install gcc-multilib g++-multilib
 	;;
     moxie-elf)
-	echo 'deb http://repos.moxielogic.org:7114/MoxieLogic moxiedev main' | sudo tee -a /etc/apt/sources.list
+	echo 'deb https://repos.moxielogic.org:7114/MoxieLogic moxiedev main' | sudo tee -a /etc/apt/sources.list
 	sudo apt-get clean # clear the cache
 	sudo apt-get update ## -qq
 	# debug...
-	curl http://repos.moxielogic.org:7114/MoxieLogic/dists/moxiedev/main/binary-amd64/Packages
+	curl https://repos.moxielogic.org:7114/MoxieLogic/dists/moxiedev/main/binary-amd64/Packages
 	sudo apt-get install -y --allow-unauthenticated moxielogic-moxie-elf-gcc moxielogic-moxie-elf-gcc-c++ moxielogic-moxie-elf-gcc-libstdc++ moxielogic-moxie-elf-gdb-sim
 	;;
   esac
