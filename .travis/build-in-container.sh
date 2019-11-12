5b297e036e61ad1ecac9c8a3e57895737c55feb7
Anthony Green <green@moxielogic.com>
2019-11-12 05:51:09 -0500

    Don't bother dumping logs.  Use rlgl instead.

--- .travis/build-in-container.sh
+++ .travis/build-in-container.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-set -x
-
-echo ===============================================================
-env
-echo ===============================================================
-
 cd /opt
 
 export QEMU_LD_PREFIX=/usr/${HOST}
@@ -14,16 +8,6 @@ export QEMU_LD_PREFIX=/usr/${HOST}
 make
 make dist
 make check RUNTESTFLAGS="-a $RUNTESTFLAGS"
-
-gzip -c -9 */testsuite/libffi.log > libffi.log.gz
-echo ================================================================
-echo The logs are too long for travis to handle, so we compress and
-echo uuencode them.  Download, decode and uncompress if you need to
-echo read them.
-echo ================================================================
-uuencode libffi.log.gz -
-echo ================================================================
-echo ================================================================
 
 ./rlgl l https://rl.gl
 ID=$(./rlgl start)
--- .travis/build.sh
+++ .travis/build.sh
@@ -32,17 +32,6 @@ function build_linux()
     make dist
     make check RUNTESTFLAGS="-a $RUNTESTFLAGS"
 
-    gzip -c -9 */testsuite/libffi.log > libffi.log.gz
-    echo ================================================================
-    echo The logs are too long for travis to handle, so we compress and
-    echo uuencode them.  Download, decode and uncompress if you need to
-    echo read them.  For example, if you select and save this text
-    echo as libffi.uu, run: 'cat libffi.uu | uudecode | gzip -d | less'.
-    echo ================================================================
-    uuencode libffi.log.gz -
-    echo ================================================================
-    echo ================================================================
-
     ./rlgl l https://rl.gl
     ID=$(./rlgl start)
     ./rlgl e --id=$ID --policy=https://github.com/libffi/rlgl-policy.git */testsuite/libffi.log
