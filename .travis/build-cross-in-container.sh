0069526c5adea9e6268b8a0087a6120c9c5c89fa
Anthony Green <green@moxielogic.com>
2019-11-28 18:58:40 -0500

    Make build errors easier to debug

--- .travis/build-cross-in-container.sh
+++ .travis/build-cross-in-container.sh
@@ -2,14 +2,11 @@
 
 cd /opt
 
-set -x
 echo $PATH
 export PATH=/usr/local/bin:$PATH
 echo $PATH
 
-ls -l /usr/local/bin
-
-./configure --host=${HOST}
+./configure --host=${HOST} || cat */config.log
 make
 make dist
 make check RUNTESTFLAGS="-a $RUNTESTFLAGS" || true
