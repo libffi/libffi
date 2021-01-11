eef85f8fdc18f8d7b6e1c1205395b2b59f4f57e8
Madhavan T. Venkataraman <madvenka@linux.microsoft.com>
2021-01-11 11:11:27 -0600

    i386: Support for Static Trampolines
    	- Define the arch-specific initialization function ffi_tramp_arch ()
    	  that returns trampoline size information to common code.
    
    	- Define the trampoline code table statically. Define two tables,
    	  actually, one with CET and one without.
    
    	- Define the trampoline code table statically.
    
    	- Introduce a tiny prolog for each ABI handling function. The ABI
    	  handlers addressed are:
    
    	  	- ffi_closure_i386
    		- ffi_closure_STDCALL
    		- ffi_closure_REGISTER
    
    	  The prolog functions are called:
    
    	  	- ffi_closure_i386_alt
    		- ffi_closure_STDCALL_alt
    		- ffi_closure_REGISTER_alt
    
    	  The legacy trampoline jumps to the ABI handler. The static
    	  trampoline jumps to the prolog function. The prolog function uses
    	  the information provided by the static trampoline, sets things up
    	  for the ABI handler and then jumps to the ABI handler.
    
    	- Call ffi_closure_tramp_init () in ffi_prep_closure_loc () to
    	  initialize static trampoline parameters.
    
    Signed-off-by: Madhavan T. Venkataraman <madvenka@linux.microsoft.com>

--- src/x86/internal.h
+++ src/x86/internal.h
@@ -27,3 +27,14 @@
 #else
 # define HAVE_FASTCALL 1
 #endif
+
+#if defined(FFI_EXEC_STATIC_TRAMP)
+/*
+ * For the trampoline code table mapping, a mapping size of 4K (base page size)
+ * is chosen.
+ */
+#define X86_TRAMP_MAP_SHIFT	12
+#define X86_TRAMP_MAP_SIZE	(1 << X86_TRAMP_MAP_SHIFT)
+#define X86_TRAMP_SIZE_CET	44
+#define X86_TRAMP_SIZE		40
+#endif
