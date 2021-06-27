baeca6c7d1d9732610c7012607cc91584577c9a4
Hood <hood@mit.edu>
2021-06-26 22:12:31 -0700

    Revert "only allocate as much space on stack for return pointer as needed"
    This reverts commit e54a30faea3803e7ac33eed191bde9e573850fc1.

--- src/wasm32/ffi.c
+++ src/wasm32/ffi.c
@@ -481,19 +481,8 @@ ffi_prep_closure_loc_helper,
     if (ret_by_arg) {
       ret_ptr = args[0];
     } else {
-      var ret_size;
-      switch (sig[ret_by_arg]) {
-      case "i":
-      case "f":
-        ret_size = 4;
-        break;
-      case "j":
-      case "d":
-        ret_size = 8;
-        break;
-      }
-      cur_ptr -= ret_size;
-      cur_ptr &= (~(ret_size - 1));
+      cur_ptr -= 8;
+      cur_ptr &= (~(8 - 1));
       ret_ptr = cur_ptr;
     }
     cur_ptr -= 4 * (sig.length - 1 - ret_by_arg);
