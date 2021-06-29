277243466a6419bd6d8275b07a1ea939a317dac8
Kleis Auke Wolthuizen <github@kleisauke.nl>
2021-06-29 15:20:00 +0200

    Emscripten: remove support for `-sWASM_BIGINT=0`

--- testsuite/emscripten/build.sh
+++ testsuite/emscripten/build.sh
@@ -16,19 +16,9 @@ mkdir -p "$TARGET"
 # Define default arguments
 DEBUG=false
 
-# JS BigInt to Wasm i64 integration, disabled by default
-# This needs to test false if there exists an environment variable called
-# WASM_BIGINT whose contents are empty. Don't use +x.
-if [ -n "${WASM_BIGINT}" ]; then
-  WASM_BIGINT=true
-else
-  WASM_BIGINT=false
-fi
-
 # Parse arguments
 while [ $# -gt 0 ]; do
   case $1 in
-    --wasm-bigint) WASM_BIGINT=true ;;
     --debug) DEBUG=true ;;
     *) echo "ERROR: Unknown parameter: $1" >&2; exit 1 ;;
   esac
@@ -37,10 +27,6 @@ done
 
 # Common compiler flags
 export CFLAGS="-O3 -fPIC"
-if [ "$WASM_BIGINT" = "true" ]; then
-  # We need to detect WASM_BIGINT support at compile time
-  export CFLAGS+=" -DWASM_BIGINT"
-fi
 if [ "$DEBUG" = "true" ]; then export CFLAGS+=" -DDEBUG_F"; fi
 export CXXFLAGS="$CFLAGS"
 
