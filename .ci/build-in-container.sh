9ae088be09feaa14435a4da85a727e78565179be
Anthony Green <green@moxielogic.com>
2021-07-04 08:42:23 -0400

    Shift from travis-ci to github actions

--- .ci/build-in-container.sh
+++ .ci/build-in-container.sh
@@ -4,6 +4,10 @@ set -x
 export QEMU_LD_PREFIX=/usr/${HOST}
 
 DEJAGNU=$(pwd)/.ci/site.exp
+echo $(DEJAGNU)
+ls -l $(DEJAGNU)
+pwd
+find .
 ./configure ${HOST+--host=$HOST --disable-shared}
 make
 make dist
