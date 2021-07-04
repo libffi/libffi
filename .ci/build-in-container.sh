fd5a529ca1ab08b8fd0c4534c9c7d5379405a637
Anthony Green <green@moxielogic.com>
2021-07-04 04:15:07 -0400

    Shift from travis-ci to github actions

--- .ci/build-in-container.sh
+++ .ci/build-in-container.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+set -x
 
 cd /opt
 
 export QEMU_LD_PREFIX=/usr/${HOST}
 
+pwd
+env
+ls -la
+
 ./configure ${HOST+--host=$HOST --disable-shared}
 make
 make dist
 make check RUNTESTFLAGS="-a $RUNTESTFLAGS" || true
-
-
