9b315d4df9814e665d26d7257d2facc7fb75c884
Hood Chatham <roberthoodchatham@gmail.com>
2023-01-27 15:46:54 -0800

    Rename emscripten quiet script

--- testsuite/emscripten/quiet_emcc_info.py
+++ testsuite/emscripten/quiet_emcc_info.py
@@ -1,15 +1,27 @@
+"""
+Prevent emcc from printing info level logging
+
+dejagnu will fail tests because of these log statements and the messages depend
+on emscripten cache state so xfailing the right tests is very hard.
+"""
+
 from pathlib import Path
 from shutil import which
+from sys import exit
 from textwrap import dedent
 
 EMCC_PATH = Path(which("emcc") + ".py")
 
 emcc_text = EMCC_PATH.read_text()
+if "# quiet_emcc_info patch" in emcc_text:
+    exit(0)
+
 emcc_lines = emcc_text.splitlines()
 emcc_lines.insert(
     1,
     dedent(
         """
+        # quiet_emcc_info patch
         import logging
         for name in ["cache", "system_libs", "shared"]:
             logger = logging.getLogger(name)
