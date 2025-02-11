91d70803adb314463405a1b7fd7660659d38b840
mengqinggang <mengqinggang@loongson.cn>
2025-02-11 17:52:12 +0800

    Add support for LoongArch32
    Change src/loongarch64 to src/loongarch.
    
    Change __loongarch64 to __loongarch_grlen.
    __loongarch64 is uesd for compatibility with legacy code,
    new programs should not assume existence of this macro.[1]
    And there is no __loongarch32 macro for LoongArch32.
    
    Add ilp32s, ilp32f, ilp32d abi.
    Add ADDI macro.
    
    [1] https://github.com/loongson/la-toolchain-conventions?tab=readme-ov-file#cc-preprocessor-built-in-macro-definitions

--- src/loongarch/ffitarget.h
+++ src/loongarch/ffitarget.h
@@ -50,20 +50,33 @@ typedef enum ffi_abi
   FFI_LP64S,
   FFI_LP64F,
   FFI_LP64D,
+  FFI_ILP32S,
+  FFI_ILP32F,
+  FFI_ILP32D,
   FFI_LAST_ABI,
 
-#if defined(__loongarch64)
-#if defined(__loongarch_soft_float)
-  FFI_DEFAULT_ABI = FFI_LP64S
-#elif defined(__loongarch_single_float)
-  FFI_DEFAULT_ABI = FFI_LP64F
-#elif defined(__loongarch_double_float)
-  FFI_DEFAULT_ABI = FFI_LP64D
+#if __loongarch_grlen == 64
+  #if defined(__loongarch_soft_float)
+    FFI_DEFAULT_ABI = FFI_LP64S
+  #elif defined(__loongarch_single_float)
+    FFI_DEFAULT_ABI = FFI_LP64F
+  #elif defined(__loongarch_double_float)
+    FFI_DEFAULT_ABI = FFI_LP64D
+  #else
+    #error unsupported LoongArch floating-point ABI
+  #endif
+#elif __loongarch_grlen == 32
+  #if defined(__loongarch_soft_float)
+    FFI_DEFAULT_ABI = FFI_ILP32S
+  #elif defined(__loongarch_single_float)
+    FFI_DEFAULT_ABI = FFI_ILP32F
+  #elif defined(__loongarch_double_float)
+    FFI_DEFAULT_ABI = FFI_ILP32D
+  #else
+    #error unsupported LoongArch floating-point ABI
+  #endif
 #else
-#error unsupported LoongArch floating-point ABI
-#endif
-#else
-#error unsupported LoongArch base architecture
+  #error unsupported LoongArch base architecture
 #endif
 } ffi_abi;
 
